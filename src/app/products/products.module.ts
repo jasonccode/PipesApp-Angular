fec5006466e9c725121d75b4db71842367270488
Jason C <grousemusic@gmail.com>
2023-12-08 18:57:02 -0500

    Módulos y Rutas de mi aplicación

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { ProductsRoutingModule } from './products-routing.module';
+
+
+@NgModule({
+  declarations: [
+    BasicsPageComponent,
+    NumbersPageComponent,
+    UncommonPageComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule
+  ]
+})
+export class ProductsModule { }
