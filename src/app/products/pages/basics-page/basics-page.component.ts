bfb95d03368ebad71bec84f63575152c0f2652df
Jason C <grousemusic@gmail.com>
2023-12-19 11:54:25 -0500

    UpperCase, LowerCase y TitleCase Pipes

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./basics-page.component.css']
 })
 export class BasicsPageComponent {
-
+  public nameLower : string = 'fernando';
+  public nameUpper : string = 'FERNANDO';
+  public fullName : string = "FerNanDo"
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,6 +4,7 @@ import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { ProductsRoutingModule } from './products-routing.module';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ProductsRoutingModule } from './products-routing.module';
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    PrimeNgModule
   ]
 })
 export class ProductsModule { }
