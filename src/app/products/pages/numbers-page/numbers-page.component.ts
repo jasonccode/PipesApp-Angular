757182259fe5c08f3f5cc507857b46a25458712f
Jason C <grousemusic@gmail.com>
2023-12-19 13:24:22 -0500

    Decimal, Currency y Percent Pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrls: ['./numbers-page.component.css']
+  styleUrls: ['./numbers-page.component.css'],
 })
 export class NumbersPageComponent {
-
+  public totalSells: number = 2567789.5567;
+  public percent: number = 0.4856;
 }
