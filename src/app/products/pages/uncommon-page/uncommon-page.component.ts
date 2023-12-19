aeb2adcb13dafcc04ad28b1cbf681b1293527157
Jason C <grousemusic@gmail.com>
2023-12-19 13:46:23 -0500

    I18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
+  // i18h Select
+  public name: string = 'Fernando';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
 }
